f13a5167645744be735404933dfd86fb20d3c848
Luis Geniole <luis.geniole@gmail.com>
2020-04-23 15:53:26 -0300

    Finish challenge.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [repos, setRepos] = useState([]);
 
   const [title, setTitle] = useState("");
+  const [tech, setTech] = useState("");
   const [techs, setTechs] = useState([]);
   const [url, setUrl] = useState("");
 
@@ -23,6 +24,10 @@ function App() {
   useEffect(() => {
     getRepos();
   }, []);
+
+  async function handleAddTech() {
+    setTechs([...techs, tech]);
+  }
 
   async function handleAddRepository() {
     try {
@@ -47,42 +52,91 @@ function App() {
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
+    <main className="content">
+      <ul className="repo_list" data-testid="repository-list">
         {repos.length > 0 &&
           repos.map((repo) => (
-            <li id={`repo-${repo.id}`} key={repo.id}>
-              {repo.title}
-              <button onClick={() => handleRemoveRepository(repo.id)}>
+            <li className="repo" id={`repo-${repo.id}`} key={repo.id}>
+              <a className="repo_link" href={repo.url}>
+                {repo.title}
+              </a>
+
+              <button
+                className="remove_repo_button"
+                onClick={() => handleRemoveRepository(repo.id)}
+              >
                 Remover
               </button>
             </li>
           ))}
       </ul>
 
-      <input
-        type="text"
-        name="title"
-        id="title"
-        valeu={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <input
-        type="text"
-        name="techs"
-        id="techs"
-        valeu={techs}
-        onChange={(e) => setTechs(e.target.value)}
-      />
-      <input
-        type="text"
-        name="url"
-        id="url"
-        valeu={url}
-        onChange={(e) => setUrl(e.target.value)}
-      />
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+      <aside className="new_repo_form">
+        <label className="repo_input_label" htmlFor="title">
+          Título
+        </label>
+        <input
+          className="repo_input"
+          type="text"
+          name="title"
+          id="title"
+          valeu={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <div className="repo_tech_input_group">
+          <label className="repo_input_label" htmlFor="tech">
+            Tecnologías
+          </label>
+
+          <span className="repo_input_tech_input_button">
+            <input
+              className="repo_tech_input"
+              type="text"
+              name="tech"
+              id="tech"
+              valeu={tech}
+              onChange={(e) => setTech(e.target.value)}
+            />
+
+            <button className="add_tech_button" onClick={handleAddTech}>
+              +
+            </button>
+          </span>
+        </div>
+
+        <label className="repo_input_label" htmlFor="url">
+          URL
+        </label>
+        <input
+          className="repo_input"
+          type="text"
+          name="url"
+          id="url"
+          valeu={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+
+        <button className="add_repo_button" onClick={handleAddRepository}>
+          Adicionar
+        </button>
+      </aside>
+
+      {techs.length > 0 && (
+        <aside className="input_techs">
+          <dl>
+            {techs.map((tech, index) => (
+              <>
+                <dt className="tech_index" id={`tech-${index}`} key={index}>
+                  {index}
+                </dt>
+                <dd>{tech}</dd>
+              </>
+            ))}
+          </dl>
+        </aside>
+      )}
+    </main>
   );
 }
 
